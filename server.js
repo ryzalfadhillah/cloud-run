9d31e27265c7ca4e6be6ea77b01c857e79afbae1
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2023-05-08 23:15:45 +0700

    Update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Cloud run testing');
+	res.send('Cloud run testing update');
 });
 
 const PORT = process.env.PORT || 8080;
