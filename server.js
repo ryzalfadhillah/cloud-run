dbffa17ba89bc701c9d781b58693e05c8a7de59c
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2023-05-08 21:59:10 +0700

    Initial

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Perubahan pertama');
+	res.send('Cloud run');
 });
 
 const PORT = process.env.PORT || 8080;
